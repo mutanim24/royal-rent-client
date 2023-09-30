29bae9d61ee43342b5eb8047c6ec83d0d889dda6
mahdee18 <mahdeec10@gmail.com>
2023-09-30 22:10:41 +0600

    Setup Auth Providers for Authentication

---
+++ src/Providers/AuthProviders.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import app from '../firebase/firebase.config';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth';
+
+import { createContext } from 'react';
+import { useState } from 'react';
+
+
+export const AuthContext = createContext(null)
+const googleProvider = new GoogleAuthProvider()
+const auth = getAuth(app)
+
+const AuthProviders = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+
+    const createUser = (email, password, displayName, photoUrl) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password).then(
+            (userCredential) => {
+                // Set display name and photo URL
+                return updateProfile(userCredential.user, {
+                    displayName,
+                    photoUrl,
+                }).then(() => {
+                    return userCredential;
+                });
+            }
+        );
+    }
+    // sing in user
+    const singIn = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    const googleSignIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    const logOut = () => {
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            console.log("logged User", loggedUser)
+            setLoading(false)
+            setUser(loggedUser)
+        })
+        return () => {
+            return unsubscribe()
+        }
+    }, []);
+    const authInfo = {
+        user,
+        loading,
+        createUser,
+        singIn,
+        googleSignIn,
+        logOut,
+    };
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProviders;
