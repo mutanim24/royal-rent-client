5cdd5c1ccf63fd80c64851c01823f80d2b567ce3
mahdee18 <mahdeec10@gmail.com>
2023-10-07 21:00:30 +0600

    Updated

--- src/Route/route.jsx
+++ src/Route/route.jsx
@@ -6,6 +6,10 @@ import HomePage from "../pages/Home/HomePage/HomePage";
 import CarPage from "../pages/CarPage/CarPage";
 import Pricing from "../pages/Pricing/Pricing";
 import AboutUs from "../pages/AboutUs/AboutUs";
+import Blog from "../pages/Blog/Blog";
+import ConatctUs from "../pages/Contact Us/ContactUs";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 
 export const router = createBrowserRouter([
@@ -20,7 +24,23 @@ export const router = createBrowserRouter([
             {
                 path: '/car/:id',
                 element: <CarPage></CarPage>,
-                loader: ({ params }) => fetch(`http://localhost:3000/car/${params.id}`)
+                loader: ({params}) => fetch(`http://localhost:3000/car/${params.id}`)
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/contact-us',
+                element: <ConatctUs></ConatctUs>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             },
             {
                 path: "/aboutUs",
