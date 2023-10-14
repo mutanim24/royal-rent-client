250032da481206d2a96d3adf9bb041c9b7bc3bce
mutanim24 <mutanim24@gmail.com>
2023-10-14 16:37:37 +0600

    filter function added in service page

--- src/pages/Service/AllServices/AllServices.jsx
+++ src/pages/Service/AllServices/AllServices.jsx
@@ -5,19 +5,31 @@ import CardForCar from '../../../components/CardForCar/CardForCar';
 
 const AllServices = () => {
     const [cars, setCars] = useState([]);
-    useEffect(()=> {
+    useEffect(() => {
         fetch('http://localhost:3000/cars')
-        .then(res => res.json())
-        .then(data => setCars(data))
+            .then(res => res.json())
+            .then(data => setCars(data))
     }, [])
+
+    const [searchResult, setSearchResult] = useState("");
+    
     return (
-        <div className='grid grid-cols-3 gap-3 p-20'>
-            {
-                cars.map(car => <CardForCar
-                    key={car._id}
-                    car={car}
-                ></CardForCar>)
-            }
+        <div>
+            <form className='text-black px-20'>
+                <input
+                    onChange={(e) => setSearchResult(e.target.value)}
+                    type="text" placeholder="Search..."
+                    className="input input-bordered w-full max-w-xs" />
+            </form>
+            <div className='grid grid-cols-3 gap-3 p-20'>
+                {
+                    cars.filter(car => searchResult === "" ? car 
+                    : car.brand.toLowerCase().includes(searchResult.toLowerCase())).map(car => <CardForCar
+                        key={car._id}
+                        car={car}
+                    ></CardForCar>)
+                }
+            </div>
         </div>
     );
 };
